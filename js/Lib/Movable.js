51762e41bc9507487479c1f0cc9e681582713837
dmcblue <dmcblue@gmail.com>
2017-08-18 22:19:00 -0400

    Movement via events for better efficiency

--- js/Lib/Movable.js
+++ js/Lib/Movable.js
@@ -9,6 +9,7 @@ var Movable =
 		if(args.position !== undefined){
 			this.move(args.position);
 		}
+		this.element.addEventListener("move", this, false);
 	};
 
 Movable.prototype = Object.create(Item.prototype);
@@ -19,13 +20,31 @@ Movable.prototype.getProposal =
 		return this.getPosition().add(direction.scale(this.speed));
 	};
 
+Movable.prototype.handleEvent =
+	function(event){
+		switch(event.type){
+			case 'move' : 
+				var position = this.getPosition().add(event.direction.scale(event.speed));
+
+				this.element.style.left = Math.round(position.x) + 'px';
+				this.element.style.top  = Math.round(position.y) + 'px';
+				
+				break;
+		}
+	};
+
 Movable.prototype.move = Item.prototype.setPosition;
 
 Movable.prototype.moveToward =
 	function(direction, speed){
+		/*
 		if(speed === undefined){speed = this.speed;}
 		var position = this.getPosition().add(direction.scale(speed));
 		
 		this.element.style.left = Math.round(position.x) + 'px';
 		this.element.style.top  = Math.round(position.y) + 'px';
+		//*/
+		if(speed === undefined){speed = this.speed;}
+		var event = new CustomEvent('move', { direction: direction, speed: speed});
+		this.element.dispatchEvent(event);
 	};
