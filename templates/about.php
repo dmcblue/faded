3cdc01e72c71b67654c0e841feb3175233d02e8d
dmcblue <dmcblue@gmail.com>
2018-05-20 11:50:28 +0200

    Add special thanks

--- templates/about.php
+++ templates/about.php
@@ -11,7 +11,24 @@
 	<p>
 		I love games like Skyrim which have intense exploration, world building and story.  But I realized it was difficult to design this type of game without the endless killing of monsters.  Without the violence, there is no challenge and it becomes <em>Sightseeing: The Game</em>.  Regardless of what you think about video game violence, it\'s undeniable game design has over-relied on the killing-mechanic to give game play challenge and prevent boredom, to the point where we barely even consider the possibility of designing these games any other way.  So this seemed like an interesting challenge from a design standpoint.
 	</p>
+	<h2>While playing</h2>
 	<p>
-		Min size : 900x600
+		Min size browser : 900x600
 	</p>
+	<h2>Code</h2>
+	<p>
+		The code is available on <a href="https://github.com/dmcblue/faded">GitHub</a>.
+	</p>
+	<h2>Special Thanks</h2>
+	<p>
+		This game was created using the following programs:
+	</p>
+	<ul>
+		<li><a href="http://famitracker.com/">FamiTracker</a></li>
+		<li><a href="https://manual.audacityteam.org/index.html/">Audacity</a></li>
+		<li><a href="https://netbeans.org/">Netbeans</a></li>
+		<li><a href="http://www.php.net/">PHP</a></li>
+		<li><a href="https://developer.mozilla.org/bm/docs/Web/JavaScript">JavaScript</a></li>
+		<li><a href="http://www.github.com/">GitHub</a></li>
+	</ul>
 </div>
